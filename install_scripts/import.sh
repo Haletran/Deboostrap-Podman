77e9a1a466bff7f0305cb2643dd696952a743657
Baptiste-Pasquier <bpasquier123@gmail.com>
2024-01-11 20:09:23 +0000

    Safer approach to the sript

--- install_scripts/import.sh
+++ install_scripts/import.sh
@@ -2,10 +2,12 @@
 
 # set podman debian container
 name=$(cat /tmp/dpm)
-cat Debian.tar.gz | podman import - "debian-$name"
-podman run -dt  "debian-$name" bash
+podman container exists "debian-$name"
+if [ $? -eq 0 ]; then echo "Container "debian-$name" already exist"; else cat Debian.tar.gz | podman import - "debian-$name"
+podman run --name "debian-$name" -dt "debian-$name" bash
 
 # cgroupv2 manager systemd enable
 #sudo loginctl enable-linger 1001
 
 #sudo rm -rf mounts/ debian-test/ Debian.tar.gz defaultNetworkBackend overlay overlay-images overlay-layers/ overlay-locks/ storage.lock tmp/ userns.lock overlay-containers
+
--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -1,21 +1,45 @@
 #!bin/sh
 
 # Variables
+sudName=$(whoami)
 tarName="Debian.tar.gz"
-sudName="dpm"
+installlib=("debootstrap" "dirmngr" "podman" "slirp4netns" "fuse-overlayfs")
 
-# add user and rootless podman
+#Functions
+run_install()
+{
+    read -p "Do you want to install missing libraries? [Y/n]: " answer
+    answer=${answer:Y}
+    [[ $answer =~ [Yy] ]] && apt-get install ${installlib[@]}
+}
+
 echo "user.max_user_namespaces=28633" > /etc/sysctl.d/userns.conf
 sysctl -p /etc/sysctl.d/userns.conf
-adduser $sudName
-usermod -aG sudo $sudName
-usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $sudName
 
+add_user()
+{
+    read -p "Do you want to add a new user? [Y/n]: " answer
+    read -p "Name of the user : " sudName
+    answer=${answer:Y}
+    [[ $answer =~ [Yy] ]] && adduser $sudName && usermod -aG sudo $sudName
+    usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $sudName
+
+}
+
+add_user
 # Dependecies install / Debootstrap tarball
-apt-get install debootstrap dirmngr podman slirp4netns fuse-overlayfs
-read -p $'\e[33mWhich version of Debian you want to install ? (bookworm,buster,bullseye,buster,stretch) :\e[0m ' codeName
-debootstrap --variant=minbase --components=main,contrib,non-free --include=sudo,git,dirmngr,apt-transport-https $codeName "debian-$codeName" http://deb.debian.org/debian/ 
-tar --verbose --create --file $tarName --directory "debian-$codeName" .
+dpkg -s "${installlib[@]}" >/dev/null 2>&1 || run_install
+if [ -f "$tarName" ]; then
+    echo "$FILE exists."
+    read -p "Do you want to remove the files? [Y/n]: " answer
+    answer=${answer:Y}
+    [[ $answer =~ [Yy] ]] && rm -rf $(tarName)
+    
+else
+    read -p $'\e[33mWhich version of Debian you want to install ? (bookworm,buster,bullseye,buster,stretch) :\e[0m ' codeName
+    debootstrap --variant=minbase --components=main,contrib,non-free-firmware --include=sudo,git,dirmngr,apt-transport-https $codeName "debian-$codeName" http://deb.debian.org/debian/
+    tar --verbose --create --file $tarName --directory "debian-$codeName" .
+fi
 
 echo $codeName > /tmp/dpm
 # launch podman import and run script as $sudName
